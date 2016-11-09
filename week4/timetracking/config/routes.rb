ca5f73c7efb75b52453d5233e02a7b37f8f23bfe
Sebe Keller <sebethkeller@gmail.com>
2016-11-09 10:36:07 -0500

    Add show method

--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 	get '/say_name/:name', to: 'site#say_name'
 	get '/calculate', to: 'calculate#add_form'
 	post '/add', to: 'calculate#add' 
+	get '/projects', to: 'projects#index', as: 'project_page'
+	get '/projects/:id', to: 'projects#show', as: 'show_project'
 end
--- week4/timetracking/db/seeds.rb
+++ week4/timetracking/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+projectA = Project.create!(name: "Ironhack")
+projectB = Project.create!(name: "Train Chasers", description: "Chasing trains")
+projectC = Project.create!(name: "Popcorn Shrimp", description: "Delicious")
+projectD = Project.create!(name: "Beach Cleanup", description: "Make it nice")
+projectE = Project.create!(name: "Bird Banding", description: "So many fluffballs!")
+projectF = Project.create!(name: "Electrofishing", description: "Electricity pack and water, what could go wrong")
+projectG = Project.create!(name: "Stocking Mountain Lakes", description: "Eject fish from from helicopter flyover")
+projectH = Project.create!(name: "Grubbing Burrows", description: "Scramble out on cliffs and reach blindly into holes in the rock you know contain wild animals")
+projectI = Project.create!(name: "Original Project Name", description: "Super origional")
+projectJ = Project.create!(name: "10th Project", description:"Now this is just ridiculous")
