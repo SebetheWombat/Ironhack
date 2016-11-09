cc7c0a9a7798c5f3c3b8512a6791a5463cb69be4
Sebe Keller <sebethkeller@gmail.com>
2016-11-09 16:02:59 -0500

    TimeEntries controller

--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 	get '/projects/new', to: 'projects#new'
 	post '/projects', to: 'projects#create'
 	get '/projects/:id', to: 'projects#show', as: 'project'
+	get '/projects/:project_id/time_entries', to: 'time_entries#index', as: 'project_time_entries'
 end
