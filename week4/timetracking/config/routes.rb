da7d0fd7300cc7bae73af7bfae4e92cc07783d8c
Sebe Keller <sebethkeller@gmail.com>
2016-11-11 09:39:17 -0500

    update routes

--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -4,14 +4,7 @@ Rails.application.routes.draw do
 	get '/say_name/:name', to: 'site#say_name'
 	get '/calculate', to: 'calculate#add_form'
 	post '/add', to: 'calculate#add' 
-	get '/projects', to: 'projects#index'
-	get '/projects/new', to: 'projects#new'
-	post '/projects', to: 'projects#create'
-	get '/projects/:id', to: 'projects#show', as: 'project'
-	get '/projects/:project_id/time_entries', to: 'time_entries#index', as: 'project_time_entries'
-	get '/projects/:project_id/time_entries/new', to: 'time_entries#new', as: 'new_project_time_entry'
-	post '/projects/:project_id/time_entries', to: "time_entries#create"
-	get '/projects/:project_id/time_entries/:id/edit', to: 'time_entries#edit', as: 'edit_project_time_entry'
-	patch '/projects/:project_id/time_entries/:id', to: 'time_entries#update', as: 'project_time_entry'
-	delete '/projects/:project_id/time_entries/:id', to: 'time_entries#destroy'
+	resources :projects, only: [:index, :show, :new, :create] do
+		resources :time_entries, except: [:show]
+	end
 end
