059aea3df7d80683496c71c62bc5e9a10cefe51a
Sebe Keller <sebethkeller@gmail.com>
2016-11-18 11:42:40 -0500

    Working spotify preview player app

---
+++ week5/spotify-skeleton-master/spotify.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+	$('.js-search').on("submit",findSong);
+	$('.btn-play').on("click", function(){
+		var player = $('.js-player');
+		if($(this).hasClass("playing")){
+			$('.js-player').trigger('pause');
+		}else{
+			$('.js-player').trigger('play');
+		}
+		$('.js-player').on('timeupdate', trackTime);
+		$(this).toggleClass("playing");
+	});	
+});
+
+function trackTime(){
+	var curTime = $('.js-player').prop('currentTime');
+	$('.js-bar').attr('value',curTime);
+}
+
+function findSong(e){
+	e.preventDefault();
+	var sInfo = $('.js-song').val();
+	var url = `https://api.spotify.com/v1/search?q=${sInfo}&type=track`;
+	$.ajax({
+		type: "GET",
+		url: url,
+		success: getSong,
+		error: handleError
+	});
+}
+
+function getSong(response){
+	if(response.tracks.items.length === 0){
+		console.log("NOOOOOOOOOOOOO");
+		var warn = `<div class="alert alert-warning" role="alert">
+							There is no song by that title
+							<button class="js-warn">OK</button></div>`
+		$('body').prepend(warn);
+		$('.js-warn').click(function(){
+			$('.alert-warning').hide();
+		})
+	} else{
+		console.log("SUCCESS");
+		var info = response.tracks.items[0];
+		console.log(info);
+		var songPrev = info.preview_url;
+		var title = info.name;
+		var artist = info.artists[0].name
+		var imgSrc = "https://i.scdn.co/image/04da6dfc1f5f45fdeba938a0cc71cf372524fd43";
+		if(info.album.images.length > 0){
+			imgSrc = info.album.images[0].url;
+		}
+		
+		var img = `<img src=${imgSrc}>`
+
+		$('.title').text(title);
+		$('.author').text(artist);
+		$('.cover').html(img);
+		$('.js-player').attr('src', songPrev);
+	}
+}
+
+function handleError(err){
+	console.log("ERROR!");
+	console.log(err);
+}
