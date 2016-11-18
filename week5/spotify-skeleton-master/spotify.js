610fedb20b8f4dd1d27081a9d54c185d389efb76
Sebe Keller <sebethkeller@gmail.com>
2016-11-18 16:08:01 -0500

    Add artist modal for current track

--- week5/spotify-skeleton-master/spotify.js
+++ week5/spotify-skeleton-master/spotify.js
@@ -10,7 +10,43 @@ $(document).ready(function(){
 		$('.js-player').on('timeupdate', trackTime);
 		$(this).toggleClass("playing");
 	});	
+	$('.author').on('click', artistLookup);
 });
+
+function artistLookup(){
+	console.log("lookup artist");
+	var artist = $('.author').data("id");
+	console.log(artist);
+	 var url = `https://api.spotify.com/v1/artists/${artist}`
+	$.ajax({
+		type: "GET",
+		url: url,
+		success: showArtist,
+		error: handleError
+	});
+}
+
+function showArtist(response){
+	console.log("ARTEEEST");
+	console.log(response);
+	var popular = response.popularity;
+	var followers = response.followers.total;
+	var genres = response.genres;
+
+	var photoSrc = "https://cdn.pixabay.com/photo/2016/11/02/17/08/music-1792286_1280.jpg";
+	if(response.images.length > 0){
+		photoSrc = response.images[0].url;
+	}
+	$('.js-modal-genres').empty();
+	genres.forEach(function(g){
+		$('.js-modal-genres').append(`<li>${g}</li>`);
+	});
+	$('.js-modal-photo').attr('src', photoSrc);
+	$('.js-modal-pop').text(popular);
+	$('.js-modal-follow').text(followers);
+	$('.js-modal-name').text(response.name);
+	$('.js-modal').modal('show');
+}
 
 function trackTime(){
 	var curTime = $('.js-player').prop('currentTime');
@@ -45,16 +81,18 @@ function getSong(response){
 		console.log(info);
 		var songPrev = info.preview_url;
 		var title = info.name;
-		var artist = info.artists[0].name
+		var artist = info.artists[0].name;
+		var artistId = info.artists[0].id;
+
 		var imgSrc = "https://i.scdn.co/image/04da6dfc1f5f45fdeba938a0cc71cf372524fd43";
 		if(info.album.images.length > 0){
 			imgSrc = info.album.images[0].url;
 		}
-		
 		var img = `<img src=${imgSrc}>`
-
 		$('.title').text(title);
 		$('.author').text(artist);
+		$('.author').data('id',artistId);
+
 		$('.cover').html(img);
 		$('.js-player').attr('src', songPrev);
 	}
