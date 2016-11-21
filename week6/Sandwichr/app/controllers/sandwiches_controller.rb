50ad01ce7a585c4da3d5a791a05d1163c70abd80
Sebe Keller <sebethkeller@gmail.com>
2016-11-21 18:13:55 -0500

    join tables

--- week6/Sandwichr/app/controllers/sandwiches_controller.rb
+++ week6/Sandwichr/app/controllers/sandwiches_controller.rb
@@ -12,8 +12,9 @@ class SandwichesController < ApplicationController
 	end
 
 	def show
-		sandwich = Sandwich.find(params[:id])
-		render json:sandwich
+		a_sandwich = Sandwich.find(params[:id])
+		many_ingredients = a_sandwich.ingredients
+		render json: {sandwich: a_sandwich, ingredients: many_ingredients}
 	end
 
 	def update
@@ -28,9 +29,20 @@ class SandwichesController < ApplicationController
 		render json: sandwich
 	end
 
+	def add_ingredient
+		the_sandwich = Sandwich.find(params[:id])
+		ah_ingredient = Ingredient.find(params[:ingredient_id])
+		#the_sandwich.ingredients.push(ah_ingredient)
+
+		SandwichIngredient.create(sandwich_id: the_sandwich.id, ingredient_id: ah_ingredient.id)
+		redirect_to "/sandwiches/#{params[:id]}"
+	end
+
 	private
 
 	def sandwich_params
 		params.require(:sandwich).permit(:name, :bread_type)
 	end
+
+
 end
